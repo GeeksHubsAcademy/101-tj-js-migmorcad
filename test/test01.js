a80913c915b63f8fbf45f8d63c0543c0a45ef433
Miguel Angel -Cheroki- <miguelangelmorato84@gmail.com>
2021-03-30 13:37:49 +0200

    test01.js completed

--- test/test01.js
+++ test/test01.js
@@ -1,4 +1,19 @@
-function clasificaElementos(index) {
-    return [0,0,0];
+function clasificaElementos(enteros) {
+    let positivos = [];
+    let negativos = [];
+    let ceros = [];
+
+    enteros.forEach(number => {
+        if (number > 0) {
+            positivos.push(number)
+        } else if (number < 0) {
+            negativos.push(number);
+        } else {
+            ceros.push(number);
+        }
+    })
+
+    let resultado = [(positivos.length / enteros.length).toFixed(4), (negativos.length / enteros.length).toFixed(4), (ceros.length / enteros.length).toFixed(4)];
+    return resultado;
 }
 module.exports = clasificaElementos;
